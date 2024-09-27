207c1a788a9b802885bdbdc03aec9b8a90e96d7b
rotimiakanni <akannirotimi@yahoo.com>
2024-09-27 09:14:47 +0100

    chore: implement ranking service in frontend to query api

---
+++ frontend/src/api/rankingService.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+// Define the base URL for the API
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+const API_KEY = process.env.REACT_APP_API_KEY;
+console.log(API_BASE_URL);
+
+/**
+ * Function to fetch rankings for a given metric.
+ * @param {string} metric - The metric to rank the LLMs by (e.g., 'ttft', 'tps', 'e2e_latency', 'rps').
+ * @returns {Promise} - A promise that resolves to the API response data.
+ */
+export const getRankingsByMetric = async (metric) => {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/rankings/${metric}`,
+      {headers: {
+        'api-key': `${API_KEY}` 
+      }}
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error)
+    throw new Error('Error fetching rankings data.');
+  }
+};
