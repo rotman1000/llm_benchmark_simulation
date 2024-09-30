4d014a8476eda9a414a62db122f8e08f42681f0b
rotimiakanni <akannirotimi@yahoo.com>
2024-09-30 01:19:36 +0100

    chore: remove prometheus since it is not completely implemented

--- api/app/main.py
+++ api/app/main.py
@@ -1,11 +1,7 @@
-import time
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 from fastapi.middleware.cors import CORSMiddleware
-from starlette.responses import Response
 from tenacity import retry, stop_after_attempt, wait_fixed
-from prometheus_client import start_http_server, Counter, Summary
-from prometheus_client import CONTENT_TYPE_LATEST, generate_latest
 from .security import get_api_key
 from .database import get_db
 from .ranking import get_rankings
@@ -23,28 +19,6 @@ app.add_middleware(
     allow_methods=["*"],            
     allow_headers=["*"],  
 )
-
-
-# Create Prometheus metrics
-REQUEST_COUNT = Counter("api_requests_total", "Total number of requests")
-REQUEST_LATENCY = Summary("request_latency_seconds", "Request latency in seconds")
-
-@app.middleware("http")
-async def prometheus_middleware(request, call_next):
-    # Increment request count
-    REQUEST_COUNT.inc()
-
-    # Measure request latency
-    start_time = time.time()
-    response = await call_next(request)
-    process_time = time.time() - start_time
-    REQUEST_LATENCY.observe(process_time)
-
-    return response
-
-@app.get("/metrics")
-def metrics():
-    return Response(generate_latest(), media_type=CONTENT_TYPE_LATEST)
 
 @retry(stop=stop_after_attempt(3), wait=wait_fixed(2))
 @app.get("/rankings/{metric_name}")
